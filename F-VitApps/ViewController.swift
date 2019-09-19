ac52f6c71a329c383abb842061a47911957b3e16
galih asmarandaru <galih.asmarandaru@gmail.com>
2019-09-19 11:40:45 +0700

    Initial Commit

---
+++ F-VitApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  F-VitApps
+//
+//  Created by Galih Asmarandaru on 19/09/19.
+//  Copyright © 2019 Galih Asmarandaru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
